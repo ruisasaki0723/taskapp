da21febc153887a829ed01033204e6999ab2cc97
ruisasaki0723 <ruitohoku96@gmail.com>
2020-04-22 01:39:48 +0900

    pod install と .gitignoreの作成まで実施

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegata = self
+        tableView.dataSource = self
     }
+    
+    //データの数を返すメソッド
+    func tableView(_ tableView: UITableView, number0fRowsInSection sectinon: Int) -> Int {
+        return 0
+    }
+    
+    // 各セルを選択した時に実行されるメソッド
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        }
 
+        // セルが削除が可能なことを伝えるメソッド
+        func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+            return .delete
+        }
 
+        // Delete ボタンが押された時に呼ばれるメソッド
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+        }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue",sender: nil) //
+    }
+        
 }
 
